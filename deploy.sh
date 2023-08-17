42c18386828de3445642687aac2d5209062cdd7c
shenshuai89 <shenshuai89@qq.com>
2023-08-17 20:36:34 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:shenshuai89/shenshuai89.github.io.git gh-pages
+git push -f git@github.com:shenshuai89/shenshuai89.github.io.git master:gh_pages
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
