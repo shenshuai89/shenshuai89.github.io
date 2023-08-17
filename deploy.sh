ce5480024bb7a963184031bd731f3c84adaaf59d
shenshuai_py <shen-sh@neusoft.com>
2023-08-17 20:23:35 +0800

    update deploy branch

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:shenshuai89/shenshuai89.github.io.git master
+git push -f git@github.com:shenshuai89/shenshuai89.github.io.git gh-pages
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
