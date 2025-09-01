a2ecdba0d08c579f8fe7660017346c8298722491
shenshuai_dr <shen-sh@neusoft.com>
2025-09-01 10:20:57 +0800

    fix: 关联谷歌广告

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -386,7 +386,7 @@ export default {
     [
       "script",
       {
-        // "data-ad-client": "ca-pub-7102294115613821",
+        "data-ad-client": "ca-pub-7102294115613821",
         async: "async",
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
       },
